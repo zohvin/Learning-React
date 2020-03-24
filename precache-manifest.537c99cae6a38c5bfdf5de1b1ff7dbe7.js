aec95e6741c97ee6e4e5203eb8ceb51dc3ccdfe5
zzorro7 <basnyatzs@gmail.com>
2020-03-25 01:51:53 +1100

    Updates

---
+++ precache-manifest.537c99cae6a38c5bfdf5de1b1ff7dbe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4058ff64b64e4e8914abd4e7672a53",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "c639527f83544d7d7e42",
+    "url": "/React-Todo/static/css/main.b0157130.chunk.css"
+  },
+  {
+    "revision": "40f1e180e5947c6cd083",
+    "url": "/React-Todo/static/js/2.66387a13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo/static/js/2.66387a13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c639527f83544d7d7e42",
+    "url": "/React-Todo/static/js/main.1cda37c1.chunk.js"
+  },
+  {
+    "revision": "3517293ca5b84827e859",
+    "url": "/React-Todo/static/js/runtime-main.90f1424b.js"
+  }
+]);
